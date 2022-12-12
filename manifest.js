ab47c19dc94456c9205663556dfd41bb02f7c042
dev2820 <second28200@gmail.com>
2022-12-13 00:46:29 +0900

    chore/ change vite.config.js for build project

---
+++ manifest.js
@@ -0,0 +1,4 @@
+import { defineManifest } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+
+export default defineManifest(manifest);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: /^lit/,
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
   },
-})
+  plugins: [crx({ manifest })],
+});
