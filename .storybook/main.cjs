c457758edcf07e612b984e44e1ce6268a3fefde1
dev2820 <second28200@gmail.com>
2022-12-14 16:01:24 +0900

    chore/ add storybook-figma addon

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,11 @@ const { resolve } = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-designs",
+  ],
   framework: "@storybook/web-components",
   core: {
     builder: "storybook-builder-vite",
