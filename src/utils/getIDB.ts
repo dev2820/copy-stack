703ef227bcd6c994d9cf9943e6938de3102f5af9
dev2820 <second28200@gmail.com>
2022-12-21 21:38:21 +0900

    feature/ add onupgradedneeded option

--- src/utils/getIDB.ts
+++ src/utils/getIDB.ts
@@ -1,4 +1,7 @@
-export default async function getIDB(name: string): Promise<IDBDatabase> {
+export default async function getIDB(
+  name: string,
+  onupgradedneeded?: (evt: IDBVersionChangeEvent) => void
+): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(name);
     request.onsuccess = () => {
@@ -7,5 +10,8 @@ export default async function getIDB(name: string): Promise<IDBDatabase> {
     request.onerror = () => {
       reject(null);
     };
+    if (onupgradedneeded) {
+      request.onupgradeneeded = onupgradedneeded;
+    }
   });
 }
