7dd5df8ebde1dd2d0e5dc88e1d5b7e3360940087
dev2820 <second28200@gmail.com>
2022-12-21 21:31:58 +0900

    refactor/ rename test folder to __test__

--- test/utils/getIDB.test.ts
+++
@@ -1,8 +0,0 @@
-import "fake-indexeddb/auto";
-import getIDB from "@/utils/getIDB";
-import { test, expect } from "@jest/globals";
-
-test("get DB", async () => {
-  const db = await getIDB("test");
-  expect(db).toBeInstanceOf(IDBDatabase);
-});
