ec56cb3d2b00c43e351b6697f6eee4ba51b1249c
dev2820 <second28200@gmail.com>
2022-12-23 01:10:22 +0900

    refactor/ change return type of BaseModel's read,readAll for immutable

--- src/classes/Model/BaseModel.ts
+++ src/classes/Model/BaseModel.ts
@@ -5,8 +5,8 @@ export default abstract class BaseModel<Data> implements CRUDable<Data> {
   public abstract create(data: Data): boolean | Promise<boolean>;
   public abstract read(
     id: number
-  ): Entity<Data> | undefined | Promise<Entity<Data> | undefined>;
-  public abstract readAll(): Entity<Data>[] | Promise<Entity<Data>[]>;
+  ): Readonly<Entity<Data> | undefined | Promise<Entity<Data> | undefined>>;
+  public abstract readAll(): Readonly<Entity<Data>[] | Promise<Entity<Data>[]>>;
   public abstract update(data: Entity<Data>): boolean | Promise<boolean>;
   public abstract delete(id: number): boolean | Promise<boolean>;
 }
