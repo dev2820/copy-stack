f8277678bbf80abbfa9b4ec94f8b3b5f1b6cbe28
dev2820 <second28200@gmail.com>
2022-12-22 14:20:57 +0900

    feature/ add id params for delete function interface

--- src/interfaces/CRUDable.ts
+++ src/interfaces/CRUDable.ts
@@ -2,5 +2,5 @@ export default interface ObjectStore<Entity> {
   create(entity: Entity): Promise<Entity[]>;
   read(id: string): Promise<Entity>;
   update(id: string, entity: Entity): Promise<Entity[]>;
-  delete(): Promise<Entity[]>;
+  delete(id: string): Promise<Entity[]>;
 }
