7a356c7469717a41dbce0077a3b87004c29ea1fc
dev2820 <second28200@gmail.com>
2022-12-23 01:03:16 +0900

    Refactor/ change type Boolean -> boolean

--- src/classes/Model/BaseModel.ts
+++ src/classes/Model/BaseModel.ts
@@ -2,9 +2,11 @@ import CRUDable from "@/interfaces/CRUDable";
 import type Entity from "@/types/Entity";
 
 export default abstract class BaseModel<Data> implements CRUDable<Data> {
-  public abstract create(data: Data): Promise<Boolean>;
-  public abstract read(id: number): Promise<Entity<Data> | undefined>;
-  public abstract readAll(): Promise<Entity<Data>[]>;
-  public abstract update(data: Entity<Data>): Promise<Boolean>;
-  public abstract delete(id: number): Promise<Boolean>;
+  public abstract create(data: Data): boolean | Promise<boolean>;
+  public abstract read(
+    id: number
+  ): Entity<Data> | undefined | Promise<Entity<Data> | undefined>;
+  public abstract readAll(): Entity<Data>[] | Promise<Entity<Data>[]>;
+  public abstract update(data: Entity<Data>): boolean | Promise<boolean>;
+  public abstract delete(id: number): boolean | Promise<boolean>;
 }
--- src/interfaces/CRUDable.ts
+++ src/interfaces/CRUDable.ts
@@ -1,8 +1,10 @@
 import type Entity from "@/types/Entity";
 export default interface ObjectStore<Data> {
-  create(data: Data): Promise<Boolean>;
-  read(id: number): Promise<Entity<Data> | undefined>;
-  readAll(): Promise<Entity<Data>[]>;
-  update(data: Entity<Data>): Promise<Boolean>;
-  delete(id: number): Promise<Boolean>;
+  create(data: Data): boolean | Promise<boolean>;
+  read(
+    id: number
+  ): Entity<Data> | undefined | Promise<Entity<Data> | undefined>;
+  readAll(): Entity<Data>[] | Promise<Entity<Data>[]>;
+  update(data: Entity<Data>): boolean | Promise<boolean>;
+  delete(id: number): boolean | Promise<boolean>;
 }
