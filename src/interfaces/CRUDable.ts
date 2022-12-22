f0898236c3f8b311d267aac487335fd81fa7442d
dev2820 <second28200@gmail.com>
2022-12-23 00:25:02 +0900

    feature/ add interface method(readAll)

--- src/interfaces/CRUDable.ts
+++ src/interfaces/CRUDable.ts
@@ -2,6 +2,7 @@ import type Entity from "@/types/Entity";
 export default interface ObjectStore<Data> {
   create(data: Data): Promise<Boolean>;
   read(id: number): Promise<Entity<Data> | undefined>;
+  readAll(): Promise<Entity<Data>[]>;
   update(data: Entity<Data>): Promise<Boolean>;
   delete(id: number): Promise<Boolean>;
 }
