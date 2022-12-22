afa24503b3e45d7c85feaa2d8b9aabfeac1bf0a2
dev2820 <second28200@gmail.com>
2022-12-22 17:05:42 +0900

    refactor/ change test from initially no entity to one entity exist

--- __test__/classes/IndexedDBModel.test.ts
+++ __test__/classes/IndexedDBModel.test.ts
@@ -1,5 +1,5 @@
 import "fake-indexeddb/auto";
-import { test, expect, beforeEach } from "@jest/globals";
+import { test, expect, beforeAll } from "@jest/globals";
 import getIDB from "@/utils/getIDB";
 import IndexedDBStore from "@/classes/IndexedDBStore";
 
@@ -21,7 +21,7 @@ const entity1: Entity = {
 
 let testStore: IndexedDBStore<Entity> | null = null;
 
-beforeEach(async () => {
+beforeAll(async () => {
   await getIDB("testDB", (evt) => {
     const _db = (evt.target as IDBOpenDBRequest).result;
     testStore = new IndexedDBStore<Entity>(_db, "testStore", {
@@ -29,32 +29,36 @@ beforeEach(async () => {
       autoIncrement: true,
     });
   });
+
+  await testStore?.create(entity1);
 });
 test("readAll entity", async () => {
   /**
-   * there is no entity now
-   */
-  const entityList = await testStore?.readAll();
-  expect(entityList).toHaveLength(0);
-});
-
-test("create entity", async () => {
-  /**
-   * create method return isSuccess condition
-   */
-  const isSuccess = await testStore?.create(entity1);
-  expect(isSuccess).toBe(true);
-
-  /**
-   * there is one entity now
+   * there is only one entity now
    */
   const entityList = await testStore?.readAll();
   expect(entityList).toHaveLength(1);
 });
 
-test("read entity", async () => {
-  await testStore?.create(entity1);
+test("create entity", async () => {
+  const newEntity = {
+    message: "i'm new",
+    date: new Date(),
+  };
+  /**
+   * create method return isSuccess condition
+   */
+  const isSuccess = await testStore?.create(newEntity);
+  expect(isSuccess).toBe(true);
 
+  /**
+   * there is two entity now
+   */
+  const entityList = await testStore?.readAll();
+  expect(entityList).toHaveLength(2);
+});
+
+test("read entity", async () => {
   /**
    * read one entity
    * entity has id property because of autoIncrement option
