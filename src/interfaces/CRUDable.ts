bb008c61b403b33c59fdb8960f8d58d09e3764e7
dev2820 <second28200@gmail.com>
2022-12-23 01:07:58 +0900

    refactor/ add Readonly to CRUDable's read,readAll for immutable

--- src/interfaces/CRUDable.ts
+++ src/interfaces/CRUDable.ts
@@ -3,8 +3,8 @@ export default interface ObjectStore<Data> {
   create(data: Data): boolean | Promise<boolean>;
   read(
     id: number
-  ): Entity<Data> | undefined | Promise<Entity<Data> | undefined>;
-  readAll(): Entity<Data>[] | Promise<Entity<Data>[]>;
+  ): Readonly<Entity<Data> | undefined | Promise<Entity<Data> | undefined>>;
+  readAll(): Readonly<Entity<Data>[] | Promise<Entity<Data>[]>>;
   update(data: Entity<Data>): boolean | Promise<boolean>;
   delete(id: number): boolean | Promise<boolean>;
 }
