707b95b51a41ff408afd8f2c1b1594e941dbcba7
dev2820 <second28200@gmail.com>
2022-12-22 16:51:33 +0900

    feature/ change interface of ObjectStore

--- src/interfaces/CRUDable.ts
+++ src/interfaces/CRUDable.ts
@@ -1,6 +1,6 @@
 export default interface ObjectStore<Entity> {
-  create(entity: Entity): Promise<Entity[]>;
-  read(id: string): Promise<Entity>;
-  update(id: string, entity: Entity): Promise<Entity[]>;
-  delete(id: string): Promise<Entity[]>;
+  create(entity: Entity): Promise<Boolean>;
+  read(id: number): Promise<Entity | undefined>;
+  update(id: number, entity: Entity): Promise<Boolean>;
+  delete(id: number): Promise<Boolean>;
 }
