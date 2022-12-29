416a3e0eaff758b847460a6d404423e68d0ecabd
dev2820 <second28200@gmail.com>
2022-12-29 16:38:01 +0900

    feature/ add Messagable interface

--- src/modules/broadcast/interfaces/Messagable.ts
+++ src/modules/broadcast/interfaces/Messagable.ts
@@ -1,3 +1,30 @@
+type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+  | BigInt64Array
+  | BigUint64Array;
+
+type StructuredClonable =
+  | Array<StructuredClonable>
+  | ArrayBuffer
+  | Boolean
+  | DataView
+  | Date
+  | Error
+  | Map<StructuredClonable, StructuredClonable>
+  | Object
+  | RegExp
+  | Set<StructuredClonable>
+  | String
+  | TypedArray;
+
 export default interface Messagable {
-  data: any;
+  [state: string]: StructuredClonable;
 }
