235c2878c54ae71790dfe85f2c4588e9730292e5
dev2820 <second28200@gmail.com>
2022-12-29 12:18:36 +0900

    feature/ add Broadcastable interface

---
+++ src/interfaces/Broadcastable.ts
@@ -0,0 +1,22 @@
+import Messagable from "@/interfaces/Messagable";
+
+/**
+ * @description
+ * send message using sender and receive message using Receiver.
+ */
+export default interface Broadcastable {
+  sender: BroadcastChannel;
+  receiver: BroadcastChannel;
+  /**
+   * @description Broadcastable can send message which have Messagable interface, if sending is success, then return true
+   * @param {Messagable} Message Broadcastable can only send messagable Messages
+   * @returns {boolean} if send is success, then return true
+   */
+  send(Message: Messagable): boolean;
+  /**
+   * @description Broadcastable can add listeners for respond to message from other Broadcastables
+   * @param {Function} cb listener to respond to messages received
+   * @returns {Function} addListener return function that can remove listener
+   */
+  addListener(cb: Function): Function;
+}
