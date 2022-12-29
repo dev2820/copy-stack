6fdafef0d08595d6a769d5ae17a91692c68ce6f2
dev2820 <second28200@gmail.com>
2022-12-29 14:02:39 +0900

    feature/ create CommunicationDevice class

---
+++ src/modules/broadcast/classes/CommunicationDevice.ts
@@ -0,0 +1,33 @@
+import Broadcastable from "../interfaces/Broadcastable";
+import MessageAddress from "../types/MessageAddress";
+import Messagable from "../interfaces/Messagable";
+export default abstract class CommunicationDevice implements Broadcastable {
+  #sender: BroadcastChannel;
+  #receiver: BroadcastChannel;
+  #listeners: Function[] = [];
+
+  constructor(address: MessageAddress) {
+    this.#sender = new BroadcastChannel(address.sender);
+    this.#receiver = new BroadcastChannel(address.receiver);
+    this.#receiver.onmessage = this.#handleMessage;
+  }
+  send(message: Messagable): boolean {
+    try {
+      this.#sender.postMessage(message);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+  addListener(listener: Function): Function {
+    this.#listeners.push(listener);
+
+    return () => {
+      this.#listeners = this.#listeners.filter((l) => l !== listener);
+    };
+  }
+  #handleMessage(evt: MessageEvent) {
+    const data = evt.data;
+    this.#listeners.forEach((l) => l(data));
+  }
+}
