c388d0a76306257a97b8c8809d88555304517cc1
dev2820 <second28200@gmail.com>
2022-12-29 16:42:14 +0900

    feature/ add handleMessage to BroadcastingStation

--- src/modules/broadcast/classes/BroadcastingStation.ts
+++ src/modules/broadcast/classes/BroadcastingStation.ts
@@ -1,19 +1,33 @@
 import CommunicationDevice from "./CommunicationDevice";
 import createUniqueChannelAddress from "../utils/createUniqueChannelAddress";
 import ChannelAddress from "../types/ChannelAddress";
+import Messagable from "../interfaces/Messagable";
+import Action from "./Action";
 
 export default class BroadcastingStation extends CommunicationDevice {
+  #store: Record<string, any>;
   #channelAddress: ChannelAddress;
 
-  constructor(stationName: string) {
+  constructor(stationName: string, store: Record<string, any>) {
     const channelAddress = createUniqueChannelAddress(stationName);
     super(channelAddress);
 
     this.#channelAddress = channelAddress;
+    this.#store = store;
   }
 
   get channelAddress(): ChannelAddress {
     return this.#channelAddress;
   }
-  protected handleMessage(evt: MessageEvent<any>): void {}
+
+  protected handleMessage(evt: MessageEvent<any>): void {
+    const action: Action = evt.data;
+    if (!Action.isAction(action)) return;
+
+    const isChanged = this.#store.dispatch(action);
+    if (!isChanged) return;
+
+    const newState: Messagable = this.#store.$state;
+    this.broadcast(newState);
+  }
 }
