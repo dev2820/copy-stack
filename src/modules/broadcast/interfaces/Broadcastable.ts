db0d10c8edb4ac9a7174e163e088e5055e97320e
dev2820 <second28200@gmail.com>
2022-12-29 16:40:25 +0900

    feature/ change Broadcastable's interface

--- src/modules/broadcast/interfaces/Broadcastable.ts
+++ src/modules/broadcast/interfaces/Broadcastable.ts
@@ -1,14 +1,10 @@
-import Messagable from "@/modules/broadcast/interfaces/Messagable";
+import type Messageable from "./Messagable";
 
-/**
- * @description
- * send message using sender and receive message using Receiver.
- */
 export default interface Broadcastable {
   /**
-   * @description Broadcastable can send message which have Messagable interface, if sending is success, then return true
-   * @param {Messagable} Message Broadcastable can only send messagable Messages
-   * @returns {boolean} if send is success, then return true
+   * @description Broadcastable can dispatch action, if sending is success, then return true
+   * @param {Message} message Broadcastable can only dispatch message
+   * @returns {boolean} if dispatch is success, then return true
    */
-  send(Message: Messagable): boolean;
+  broadcast(message: Messageable): boolean;
 }
