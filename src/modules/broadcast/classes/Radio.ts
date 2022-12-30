9c1481801d5d28119df4bf5ec62f1bf8c2f63a0f
dev2820 <second28200@gmail.com>
2022-12-30 15:13:27 +0900

    feature/ add Offer concept, offer requests initing State of Radio

--- src/modules/broadcast/classes/Radio.ts
+++ src/modules/broadcast/classes/Radio.ts
@@ -1,10 +1,16 @@
 import MessageAddress from "../types/ChannelAddress";
 import CommunicationDevice from "./CommunicationDevice";
+import Action from "./Action";
+import Packet from "./Packet";
+import * as PACKET_TYPE from "../constants/PACKET_TYPES";
+
 export default class Radio extends CommunicationDevice {
   protected listeners: Function[] = [];
+  $state: Record<string, any> = {};
 
   constructor(address: MessageAddress) {
     super(address);
+    this.broadcast(Packet.DISCOVER);
   }
   /**
    * @description Radio can add listeners for respond to message from other CommunicationDevices
@@ -19,8 +25,26 @@ export default class Radio extends CommunicationDevice {
     };
   }
 
-  protected handleMessage(evt: MessageEvent<any>): void {
-    const newState = evt.data;
-    this.listeners.forEach((l) => l(newState));
+  broadcastAction(action: Action) {
+    const packet = new Packet({ type: PACKET_TYPE.ACTION }, action);
+    this.broadcast(packet);
+  }
+
+  protected handlePacket(packet: Packet): void {
+    if (packet.header.type === PACKET_TYPE.OFFER) {
+      this.initState(packet.payload as Record<string, any>);
+    }
+    if (packet.header.type === PACKET_TYPE.NEW_STATE) {
+      this.updateState(packet.payload as Record<string, any>);
+    }
+  }
+
+  initState(state: Record<string, any>) {
+    this.$state = state;
+  }
+
+  updateState(newState: Record<string, any>) {
+    this.$state = newState;
+    this.listeners.forEach((l) => l(this.$state));
   }
 }
--- src/modules/broadcast/constants/PACKET_TYPES.ts
+++ src/modules/broadcast/constants/PACKET_TYPES.ts
@@ -1,5 +1,6 @@
 export const ACTION = 0;
 export const NEW_STATE = 1;
 export const DISCOVER = 2;
+export const OFFER = 3;
 
-export default [ACTION, NEW_STATE, DISCOVER] as const;
+export default [ACTION, NEW_STATE, DISCOVER, OFFER] as const;
