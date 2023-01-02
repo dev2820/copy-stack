ac2b432cfbd303279c7053ab439d2ddcd7a8a265
dev2820 <second28200@gmail.com>
2023-01-02 20:46:21 +0900

    feature/ add context menu for coping image

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,11 @@
+import createCopy from "@/utils/createCopy";
 import copyStore from "@/stores/copyStore";
 import { BroadcastingStation } from "@/modules/broadcast";
-import RUNTIME_MESSAGE from "./constants/RUNTIME_MESSAGE";
+import RUNTIME_MESSAGE from "@/constants/RUNTIME_MESSAGE";
+import CONTEXT_MENUS from "@/constants/CONTEXT_MENUS";
 
 const broadcastingStation = new BroadcastingStation("copy", copyStore);
+
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
   switch (message.type) {
     case RUNTIME_MESSAGE.GET_CHANNEL_ADDRESS: {
@@ -18,4 +21,29 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
   }
 
   return true;
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: CONTEXT_MENUS.STORE_TO_COPY_STACK.ID,
+    title: CONTEXT_MENUS.STORE_TO_COPY_STACK.TITLE,
+    contexts: CONTEXT_MENUS.STORE_TO_COPY_STACK.CONTEXTS,
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info) => {
+  if (info.menuItemId !== CONTEXT_MENUS.STORE_TO_COPY_STACK.ID) return false;
+
+  if (info.selectionText) {
+    const copy = createCopy(info.selectionText, new Date(), info.pageUrl);
+    copyStore.addCopy(copy);
+  } else if (info.mediaType === "image" && info.srcUrl) {
+    const image = await fetch(info.srcUrl);
+    const blob = await image.blob();
+
+    const copy = createCopy(blob, new Date(), info.pageUrl);
+    copyStore.addCopy(copy);
+  }
+
+  return false;
 });
---
+++ src/constants/CONTEXT_MENUS.ts
@@ -0,0 +1,7 @@
+export default {
+  STORE_TO_COPY_STACK: {
+    TITLE: "store to Copy Stack",
+    ID: "Store",
+    CONTEXTS: ["selection", "image"],
+  },
+};
