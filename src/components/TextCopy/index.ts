f5ea050a5b12dad9255e7e6ba2a055759deede03
dev2820 <second28200@gmail.com>
2023-01-04 11:57:15 +0900

    feature/ create Copy button in TextCopy

--- src/components/TextCopy/index.ts
+++ src/components/TextCopy/index.ts
@@ -1,7 +1,7 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import type Copy from "@/types/Copy";
-
+import clipboardSystem from "@/modules/clipboardSystem";
 import "@/components/FilledCard";
 
 @customElement("text-copy")
@@ -13,15 +13,22 @@ export default class TextCopy extends LitElement {
     super();
   }
   render() {
-    console.log(this.copy);
     return html`
       <header>
         <h4 class="title">${this.copy.source}</h4>
         <small class="created">${this.copy.created}</small>
       </header>
-      <p>${this.copy.content}</p>
-      <button onclick="">copy</button>
+      <article>
+        <p>${this.copy.content}</p>
+      </article>
+      <menu type="list">
+        <button class="copy" @click=${() => this.#handleCopy()}>copy</button>
+      </menu>
     `;
+  }
+
+  #handleCopy() {
+    clipboardSystem.toClipboard(this.copy.content);
   }
 
   static styles = css``;
