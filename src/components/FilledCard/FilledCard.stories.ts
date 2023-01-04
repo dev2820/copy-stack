db087cc674b98bdc8d88a6d1b4dc653cf821f4d9
dev2820 <second28200@gmail.com>
2023-01-04 12:40:13 +0900

    feature/ add filled-card with image-copy case

--- src/components/FilledCard/FilledCard.stories.ts
+++ src/components/FilledCard/FilledCard.stories.ts
@@ -2,11 +2,13 @@ import { Story, Meta } from "@storybook/web-components";
 import { html, TemplateResult } from "lit-html";
 import copyList from "@mocks/copyList";
 import "@/components/TextCopy";
+import "@/components/ImageCopy";
 import "@/components/FilledCard";
 
 const noImageUrl = new URL("@/assets/images/no-image.png", import.meta.url)
   .href;
 const textCopyMock = copyList[0];
+const ImageCopyMock = copyList[1];
 
 export default {
   title: "FilledCard",
@@ -42,4 +44,8 @@ Image.args = {
 export const TextCopy = Template.bind({});
 TextCopy.args = {
   content: html`<text-copy .copy=${textCopyMock}></text-copy>`,
+};
+export const ImageCopy = Template.bind({});
+ImageCopy.args = {
+  content: html`<image-copy .copy=${ImageCopyMock}></image-copy>`,
 };
