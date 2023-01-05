d58a3a888544955ea9e47caa1b225506f7714b59
dev2820 <second28200@gmail.com>
2023-01-05 21:48:19 +0900

    feature/ change delete button's button tag to text-button

--- types/src/components/CopiedItem/index.d.ts
+++ types/src/components/CopiedItem/index.d.ts
@@ -2,6 +2,7 @@ import { LitElement } from "lit";
 import type Copy from "@/types/Copy";
 import "@/components/FilledCard";
 import "@/components/FilledButton";
+import "@/components/TextButton";
 export default class CopiedItem extends LitElement {
     #private;
     copy: Copy;
