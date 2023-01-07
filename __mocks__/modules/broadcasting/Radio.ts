09973f47ca15421a0838b929b03b0526d69072c5
dev2820 <second28200@gmail.com>
2023-01-07 22:47:46 +0900

    refactor/ set decorator for situation that copyList is empty

--- __mocks__/modules/broadcasting/Radio.ts
+++ __mocks__/modules/broadcasting/Radio.ts
@@ -1,13 +1,22 @@
 import copyList from "../../copyList";
 import {type ChannelAddress} from "broadcasting";
 
+let data = {copyList:copyList};
+
 export default class Radio {
-  initialState = copyList;
   constructor(address: ChannelAddress, initializer: Function) {
-    initializer({ copyList });
+    initializer(data);
   }
 
   $subscribe() {
     // empty
   }
+}
+
+export function decorator(story, { parameters }) {
+  if(parameters && parameters.store) {
+    data = parameters.store.data;
+  }
+
+  return story();
 }
