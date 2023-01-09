d2de3aa71d254ed0cea19e7b5ad7537b40b390d0
dev2820 <second28200@gmail.com>
2023-01-09 14:43:24 +0900

    chore/ add types

---
+++ types/src/modules/Messenger.d.ts
@@ -0,0 +1,5 @@
+import type Message from "@/types/Message";
+declare const _default: {
+    sendMessage<ReturnType_1>(message: Message): Promise<ReturnType_1>;
+};
+export default _default;
---
+++ types/src/types/Message.d.ts
@@ -0,0 +1,5 @@
+type Message = {
+    type: string;
+    payload?: any;
+};
+export default Message;
