5d53cdfb8de3a7deb22d4458f8267b67fe325cce
dev2820 <second28200@gmail.com>
2023-01-11 14:37:45 +0900

    style/ change FilledChip's style

--- src/components/FilledChip/index.ts
+++ src/components/FilledChip/index.ts
@@ -36,5 +36,34 @@ export default class FilledChip extends LitElement {
   #changeHandler() {
     this.checked = !this.checked;
   }
-  static styles = css``;
+  static styles = css`
+    label {
+      box-sizing: border-box;
+      height: 2rem;
+      padding: 0.375rem 1rem 0.375rem 1rem;
+      background: var(--filled-surface-color);
+      border-radius: var(--radius);
+      cursor: pointer;
+      display: inline-flex;
+      flex-direction: row;
+      line-height: 1.2rem;
+      user-select: none;
+    }
+    label.checked {
+      background: var(--primary-color);
+      padding: 0.375rem 1rem 0.375rem 0.5rem;
+    }
+    label > img {
+      width: 1rem;
+      height: 1rem;
+      margin: auto 0;
+      margin-right: 0.625rem;
+    }
+    input[type="checkbox"] {
+      opacity: 0;
+      width: 0px;
+      height: 0px;
+      margin: 0;
+    }
+  `;
 }
