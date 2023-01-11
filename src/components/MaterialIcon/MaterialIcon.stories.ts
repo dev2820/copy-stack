21b18862212a0f50c88b050fb95e4d42505ce63a
dev2820 <second28200@gmail.com>
2023-01-11 14:54:46 +0900

    feature/ add MaterialIcon component used to separate loading icon logic

---
+++ src/components/MaterialIcon/MaterialIcon.stories.ts
@@ -0,0 +1,31 @@
+import { Story, Meta } from "@storybook/web-components";
+import { html, TemplateResult } from "lit-html";
+
+import "@/components/MaterialIcon";
+
+export default {
+  title: "MaterialIcon",
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Llbx0pdb3UJZe8khrDHrEQ/Design?node-id=274%3A937&t=QGzmpvrhxLtkgNt2-1",
+    },
+  },
+} as Meta;
+
+type Args = {
+  icon: string;
+};
+
+const Template: Story<Args> = (args: Args) =>
+  html`<material-icon icon="${args.icon}"></material-icon>`;
+
+export const Empty = Template.bind({});
+Empty.args = {
+  icon: "",
+};
+
+export const Check = Template.bind({});
+Check.args = {
+  icon: "check",
+};
