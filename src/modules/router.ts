61333ed585e52c147b44f618e0414f84ee1ab1fa
dev2820 <second28200@gmail.com>
2023-01-12 21:13:23 +0900

    feature/ create router

---
+++ src/modules/router.ts
@@ -0,0 +1,69 @@
+import { TemplateResult } from "lit-html";
+
+type Route = {
+  path: string;
+  page: () => TemplateResult;
+};
+
+const hash2path = (hash: string) => {
+  const hashExp = /^#?(.*)/;
+  const matches = hash.match(hashExp);
+
+  if (!matches || !matches[1]) return undefined;
+  return matches[1];
+};
+
+const findRoute = (path: string) => {
+  return routeInfos.routes.find((route) => {
+    const pathExp = new RegExp(route.path);
+    return pathExp.test(path);
+  });
+};
+
+const updateLocationInfos = (path: string) => {
+  locationInfos.currentPath = path;
+  const currentRoute = findRoute(path) ?? null;
+  if (currentRoute) {
+    locationInfos.currentPage = currentRoute.page;
+  }
+};
+
+const routeInfos: {
+  routes: Route[];
+} = {
+  routes: [],
+};
+const locationInfos: {
+  currentPath: string | undefined;
+  currentPage: null | (() => TemplateResult);
+} = {
+  currentPath: "",
+  currentPage: null,
+};
+
+const _listeners: Function[] = [];
+
+window.addEventListener("hashchange", () => {
+  const path = hash2path(window.location.hash);
+
+  if (!path) {
+    throw Error("path is wrong");
+  }
+
+  updateLocationInfos(path);
+  _listeners.forEach((l) => l());
+});
+
+export default {
+  location: locationInfos,
+  init(routes: Route[]) {
+    window.location.hash = "/";
+    routeInfos.routes = routes;
+  },
+  go: (newPath: string) => {
+    window.location.hash = newPath;
+  },
+  subscribe: (listener: Function) => {
+    _listeners.push(listener);
+  },
+};
