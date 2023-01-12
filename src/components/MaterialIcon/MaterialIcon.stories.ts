6aad39d817501d03f539630075801681f16f8b54
dev2820 <second28200@gmail.com>
2023-01-13 00:52:59 +0900

    feature/ add medium size to MaterialIcon

--- src/components/MaterialIcon/MaterialIcon.stories.ts
+++ src/components/MaterialIcon/MaterialIcon.stories.ts
@@ -1,6 +1,7 @@
 import { Story, Meta } from "@storybook/web-components";
 import { html } from "lit-html";
-
+import * as ICON_SIZE from "@/constants/ICON_SIZE";
+import type IconSize from "@/types/IconSize";
 import "@/components/MaterialIcon";
 
 export default {
@@ -11,26 +12,36 @@ export default {
       url: "https://www.figma.com/file/Llbx0pdb3UJZe8khrDHrEQ/Design?node-id=274%3A937&t=QGzmpvrhxLtkgNt2-1",
     },
   },
+  argTypes: {
+    size: {
+      options: ICON_SIZE,
+      control: { type: "radio" },
+    },
+  },
 } as Meta;
 
 type Args = {
   icon: string;
+  size: IconSize;
 };
 
 const Template: Story<Args> = (args: Args) =>
-  html`<material-icon icon="${args.icon}"></material-icon>`;
+  html`<material-icon size="${args.size}" icon="${args.icon}"></material-icon>`;
 
 export const Empty = Template.bind({});
 Empty.args = {
   icon: "",
+  size: ICON_SIZE.SMALL,
 };
 
 export const Check = Template.bind({});
 Check.args = {
   icon: "check",
+  size: ICON_SIZE.SMALL,
 };
 
 export const ArrowBack = Template.bind({});
 ArrowBack.args = {
   icon: "arrow-back",
+  size: ICON_SIZE.SMALL,
 };
--- src/components/MaterialIcon/index.ts
+++ src/components/MaterialIcon/index.ts
@@ -1,35 +1,60 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import iconSprite from "@/assets/images/icon-sprite.png";
+import type IconSize from "@/types/IconSize";
+import * as ICON_SIZE from "@/constants/ICON_SIZE";
+import iconSprite16 from "@/assets/images/icon-sprite-16.png";
+import iconSprite32 from "@/assets/images/icon-sprite-32.png";
+import iconSprite48 from "@/assets/images/icon-sprite-48.png";
 
 @customElement("material-icon")
 export default class FilledChip extends LitElement {
   @property({ type: String })
   icon: string = "none";
 
+  @property({ type: String })
+  size: IconSize = ICON_SIZE.SMALL;
+
   render() {
     return html`<span
-      class="icon ${this.icon}"
-      style="background-image:url(${iconSprite})"
+      class="icon ${this.icon} ${this.size}"
+      style="background-image:url(${this.getSprite()})"
     ></span>`;
   }
 
+  getSprite() {
+    if (this.size === ICON_SIZE.LARGE) {
+      return iconSprite48;
+    }
+    if (this.size === ICON_SIZE.MEDIUM) {
+      return iconSprite32;
+    }
+    return iconSprite16;
+  }
+
   static styles = css`
-    :host {
-      --size: 3rem;
+    .small {
+      --unit: 1rem;
+    }
+    .medium {
+      --unit: 2rem;
+    }
+    .large {
+      --unit: 3rem;
     }
     .icon {
       display: inline-block;
-      width: var(--size);
-      height: var(--size);
+    }
+    .icon {
+      width: var(--unit);
+      height: var(--unit);
     }
     .icon.none {
     }
     .icon.check {
-      background-position: calc(-1 * var(--size)) 0rem;
+      background-position: calc(-1 * var(--unit)) 0rem;
     }
     .icon.arrow-back {
-      background-position: calc(-2 * var(--size)) 0rem;
+      background-position: calc(-2 * var(--unit)) 0rem;
     }
   `;
 }
