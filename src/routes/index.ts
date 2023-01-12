87162c9c7642eed27da14a82a13d03e314879308
dev2820 <second28200@gmail.com>
2023-01-13 01:18:44 +0900

    refactor/ remove router's name (not needed)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,11 @@ import { html } from "lit-html";
 import "@/pages/MainPage";
 
 export const mainPage = {
-  name: "main",
   path: "/",
   page: () => html`<main-page></main-page>`,
 };
 
 export const detailPage = {
-  name: "detail",
   path: "/:id",
   page: () => html`<detail-page></detail-page>`,
 };
